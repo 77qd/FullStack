0e4f209d51cd39fac8a170743e250f14ce695b36
Szymon Andrzejczak <andrzejczaks@tlimc.local>
2026-03-13 13:33:46 +0100

    zadanie 2.5

---
+++ part2/src/2.5/components/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ courseName }) => <h2>{courseName}</h2>
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
+  return <b>total of {total} exercises</b>
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
