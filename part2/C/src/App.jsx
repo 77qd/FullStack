79f5fd88e8efd99cdd51de6bb0ab00e1a4e7c76b
Szymon Andrzejczak <andrzejczaks@tlimc.local>
2026-05-29 13:27:22 +0200

    zmiana

---
+++ part2/C/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.id}>{person.name} {person.number}</li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
